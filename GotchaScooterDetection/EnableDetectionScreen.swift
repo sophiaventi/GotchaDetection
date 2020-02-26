a8ca223ddca1130b27e706add1940f31c9324572
Muridi <muridima@msu.edu>
2020-02-26 17:03:19 -0500

    Created first view

---
+++ GotchaScooterDetection/EnableDetectionScreen.swift
@@ -0,0 +1,23 @@
+//
+//  EnableDetectionScreen.swift
+//  GotchaScooterDetection
+//
+//  Created by Muridi, Mamduh on 2/26/20.
+//  Copyright © 2020 AESC Capstone Team 25. All rights reserved.
+//
+
+import UIKit
+
+class EnableDetectionScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    @IBAction func startDetection(_ sender: UIButton) {
+        // Start Bluetooth Scanning
+        // switch screen
+    }
+    
+}
