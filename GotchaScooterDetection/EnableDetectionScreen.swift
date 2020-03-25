8f16cb75437cddabf0179786e0836659903986fb
Mamduh Muridi <muridima@msu.edu>
2020-03-25 11:37:26 -0400

    Added BLE scanning code, Bluetooth Access request is still not complete

--- GotchaScooterDetection/EnableDetectionScreen.swift
+++ GotchaScooterDetection/EnableDetectionScreen.swift
@@ -7,13 +7,36 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class EnableDetectionScreen: UIViewController {
-
+class EnableDetectionScreen: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    
+    private var centralManager: CBCentralManager!
+    var peripheral: CBPeripheral?
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn{
+            print("Bluetooth is On")
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        }
+        else{
+            print("Bluetooth is not active")
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
 
-        
+        print("\nName    :\(peripheral.name ?? "(No name)")")
+        print("RSSI :\(RSSI)")
+        for ad in advertisementData{
+            print("AD Data: \(ad)")
+        }
     }
     @IBAction func startDetection(_ sender: UIButton) {
         // Start Bluetooth Scanning
